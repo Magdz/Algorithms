516ef1e02ad90eef895743338a46f461a1c68af5
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2016-06-01 18:11:07 +0200

    Implement Quick Union

---
+++ Union Find/Quick Union/QuickUnion.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ahmed
+ */
+public class QuickUnion {
+    
+    private final int N;
+    private final int []id;
+    
+    public QuickUnion(int N){
+        this.N = N;
+        id = new int[N];
+        for(int i=0; i < N; ++i){
+            id[i] = i;
+        }
+    }
+    
+    private int root(int i){
+        while(i != id[i]) i = id[i];
+        return i;
+    }
+    
+    public void union(int x, int y){
+        int i = root(x);
+        int j = root(y);
+        id[i] = j;
+    }
+    
+    public boolean connected(int x, int y){
+        return root(x) == root(y);
+    }
+    
+}
